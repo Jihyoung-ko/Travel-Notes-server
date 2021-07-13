cc191a9520e2b2e662fa7e54f5420cf3afd6a4ed
koji <koji4386@gmail.com>
2021-07-13 20:26:30 +0200

    edit album model

---
+++ models/Album.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const albumSchema = new Schema(
+	{
+		title: { type: String, required: true },
+		startDate: Date,
+		endDate: Date,
+		photo: String,
+		user: { type: Schema.Types.ObjectId, ref: 'User' },
+		article: [{ type: Schema.Types.ObjectId, ref: 'Article' }],
+	},
+	{
+		timestamps: {
+			createdAt: 'created_at',
+			updatedAt: 'updated_at',
+		},
+	}
+);
+
+const Album = mongoose.model('User', albumSchema);
+
+module.exports = Album;
