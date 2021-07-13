09869004aef6febb2b65c0999e853c3145ce851f
koji <koji4386@gmail.com>
2021-07-13 19:29:17 +0200

    change user model

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const { Schema } = mongoose;
 const userSchema = new Schema(
 	{
 		username: { type: String, required: true, unique: true },
-		email: { type: String, required: true, unique: true },
 		hashedPassword: { type: String, required: true },
 		album: [{ type: Schema.Types.ObjectId, ref: 'Album' }],
 	},
