2484bc554c7df98b9b30c769a5292e9a899e12e0
koji <koji4386@gmail.com>
2021-07-13 17:18:13 +0200

    add initial routes for album and article

--- routes/album.js
+++ routes/album.js
@@ -2,9 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
+// Get all Albums of user -private
 router.get('/', (req, res) => {
 	res.status(200).json({
 		msg: 'this is album route',
+	});
+});
+
+// Add new album
+router.post('/', (req, res) => {
+	res.status(200).json({
+		msg: 'Add new album',
+	});
+});
+
+// Update album
+router.put('/:id', (req, res) => {
+	res.status(200).json({
+		msg: 'Update album',
+	});
+});
+
+// Delete album
+router.delete('/:id', (req, res) => {
+	res.status(200).json({
+		msg: 'Delete album',
 	});
 });
 
--- routes/article.js
+++ routes/article.js
@@ -8,4 +8,26 @@ router.get('/', (req, res) => {
 	});
 });
 
+// Add new article
+router.post('/', (req, res) => {
+	res.status(200).json({
+		msg: 'Add new article',
+	});
+});
+
+// Update article
+router.put('/:id', (req, res) => {
+	res.status(200).json({
+		msg: 'Update article',
+	});
+});
+
+// Delete article
+router.delete('/:id', (req, res) => {
+	res.status(200).json({
+		msg: 'Delete article',
+	});
+});
+
+
 module.exports = router;
